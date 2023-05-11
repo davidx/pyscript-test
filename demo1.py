37489a411e89234f118929d667c422fc897b3463
David Andersen <davidx@gmail.com>
2023-05-11 14:26:25 -0700

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -3,16 +3,19 @@ import random
 students = ['Tomas','Sher','Sal','Jose','Ryan','Lawrence','Mel','Senay','Christie','Vincent','Caitlyn','Adeline','Ray','Mike']
 
 def pick(my_students):
+    """Removes and returns a random student from the list."""
     my_students.remove(student := random.choice(my_students))
     return student
 
-more = 'y'
+def interact_with_user(students):
+    """Interacts with the user to pick a winner."""
+    more = 'y'
+    while students and len(more) and not more[0] in 'nN':
+        winner = pick(students)
+        print("And the winner is: " + winner + ")
+        more = input("More? [y]/n: ")
+        if not len(more):
+            more = 'y'
 
-while students and len(more) and not more[0] in 'nN':
-    winner = pick(students)
-    print("And the winner is: " + winner + "!")
-    more = input("More? [y]/n: ")
-    if not len(more):
-        more = 'y'
-
+interact_with_user(students)
 print('Thanks for playing!')
